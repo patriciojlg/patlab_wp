cdf086d93c3ce711b39c1bc3aa42ad0fdf0ad26d
root <root@patlab.cl>
2017-10-18 11:50:08 -0300

    blog-layout avances

--- wp-content/themes/html5blank-stable/single.php
+++ wp-content/themes/html5blank-stable/single.php
@@ -35,16 +35,11 @@
 
 			<?php endif; ?>
 
-
-
-          
-			<!-- /post thumbnail -->
-
-
-
-  <div class="container">
+ <div class="container">
  <div class="row">
-      <div class="white col m8 s12">
+      <div class="col m8 s12">
+<!-- contenido -->
+<div class="white content">
 <!-- post title -->
 			<h1>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
@@ -52,19 +47,23 @@
 			<!-- /post title -->
        
 			<?php the_content(); // Dynamic Content ?>
-      </div>
-<div class="container">
+
+
+</div>
+<!--  inicia Sub-contenido -->
+
+ 
+<div class="bg_noise1 subcontenido">
 
 			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+			
 			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
 			<!-- /post details -->
 
 
 			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
 
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+			
 
 			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
 
@@ -72,12 +71,12 @@
 
 			<?php comments_template(); ?>
 
-		</article>
-		<!-- /article -->
-
 	<?php endwhile; ?>
 
 	<?php else: ?>
+
+
+
 
 		<!-- article -->
 		<article>
@@ -89,10 +88,17 @@
 
 	<?php endif; ?>
 
+
+
+     
+			<!-- /post thumbnail -->
+
+
+<!-- FIN Sub-contenido -->
 </div>
 
-
-      <div   class="col m4 s12">  
+</div>
+<div   class="col m4 s12">  
 <div style="padding: 1em;"  class="cv-post center-align blue">
          <img style="height: 120px; widht: 120px;" src="http://patlab.cl/wp-content/uploads/2017/09/plabarca.jpg" alt="" class="circle">
          <h5 class="white-text">Patricio Labarca</h5>
@@ -100,14 +106,15 @@
          <p class="white-text">Me especializo en psicología clínica para adultos y adolescentes. Como también en tratamiento adicciones y peritajes. </p>
 </div>
 <?php get_sidebar() ?>
-</div>
 
 </div>
-   
 
-  </div>
- 
+<!-- end content -->
 
+
+
+
+      
 
 
 
